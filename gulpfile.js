ee70dbac5f88859d01a3628f844c3d0b7b557d6f
Jacob Berger <jberger.public@live.com>
2014-06-05 21:36:21 +0200

    rename, reorder gulpfile build, clean, etc. (add gulp-clean dep);

--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,41 @@ var gutil = require('gulp-util');
 var shell = require('gulp-shell');
 var debug = require('gulp-debug');
 var extReplace = require('gulp-ext-replace');
+var clean = require('gulp-clean');
 
 var jsDeps = 'js/vendor/',
     distRoot = '_dist/';
 
 var paths =
-{
-    root: './',
-    jsDeps: jsDeps,
-    data: {
-        allFiles: 'data/*'
-    },
-    deps: [
-        'bower_components/ember/ember.js',
-        'bower_components/ember-data/ember-data.js',
-        'bower_components/handlebars/handlebars.js',
-        'bower_components/jquery/jquery.js',
-        'bower_components/markdown/lib/markdown.js'
-    ],
-    dist: {
-        root: distRoot,
-        jsDeps: distRoot + jsDeps,
-        src: [
-            // assets
-            'assets/**/*.*',
-            'css/**/*',
-            'images/**/*.*',
-            'js/app.js',
-            // index
-            'index.html'
-        ]
+    {
+        root: './',
+        jsDeps: jsDeps,
+        data: {
+            allFiles: 'data/*'
+        },
+        deps: [
+            'bower_components/ember/ember.js',
+            'bower_components/ember-data/ember-data.js',
+            'bower_components/handlebars/handlebars.js',
+            'bower_components/jquery/jquery.js',
+            'bower_components/markdown/lib/markdown.js'
+        ],
+        dist: {
+            root: distRoot,
+            recurseAll: distRoot + '**/*',
+            jsDeps: distRoot + jsDeps,
+            src: [
+                // assets
+                'assets/**/*.*',
+                'css/**/*',
+                'images/**/*.*',
+                'js/app.js',
+                // index
+                'index.html'
+            ]
+        }
     }
-};
+    ;
 
 gulp.task(
     'default',
@@ -73,16 +76,6 @@ gulp.task(
 );
 
 gulp.task(
-    'init',
-    function()
-    {
-        gulp.start('deps');
-        gulp.start('dataFiles');
-        console.log('serve via: "python -m SimpleHTTPServer 8080"');
-    }
-);
-
-gulp.task(
     'watch',
     function()
     {
@@ -98,15 +91,31 @@ gulp.task(
 );
 
 gulp.task(
-    'test',
+    'clean',
     function()
     {
-        //gulp.start('init');
+        console.log('Deleting dist files...');
+        gulp.src(paths.dist.recurseAll, { read: false })
+            .pipe(clean({ force: true }));
+        console.log('Deleted dist files.');
+    }
+);
+
+gulp.task(
+    'copy', ['clean'],
+    function()
+    {
         console.log('Copying assets, data, app files...');
         gulp.src(paths.dist.src, { base: "." })
             .pipe(gulp.dest(paths.dist.root))
         ;
-        gulp.start('init');
+        gulp.start('deps');
+        gulp.start('dataFiles');
         console.log('Copied assets, data, app files.');
     }
 );
+
+gulp.task('build', ['clean', 'copy'], function()
+{
+    console.log('serve via: "python -m SimpleHTTPServer 8080"');
+});
